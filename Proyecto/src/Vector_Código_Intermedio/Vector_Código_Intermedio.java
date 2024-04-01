ebcfa290e79a96ba0b4926b079995c1cc6c1aca5
Rogeliogamer <130010925+Rogeliogamer@users.noreply.github.com>
2024-04-01 13:52:16 -0600

    Funciones para operadores de prioridad
    Se agregaron nuevos case para manejar los operadores de prioridad y a su respectivo método.

--- Proyecto/src/Vector_Código_Intermedio/Vector_Código_Intermedio.java
+++ Proyecto/src/Vector_Código_Intermedio/Vector_Código_Intermedio.java
@@ -18,6 +18,7 @@ import java.util.Stack;
 public class Vector_Código_Intermedio {
     public static Stack<String> pilaDeOperadores = new Stack<>();
     public static Stack<Integer> pilaDePrioridad = new Stack<>();
+    public static Stack<String> pilaDeOperadoresTokens = new Stack<>();
     
     public static Stack<String> pilaDeEstatutos = new Stack<>();
     public static Stack<Integer> pilaDeDirecciones = new Stack<>();
@@ -27,7 +28,7 @@ public class Vector_Código_Intermedio {
     
     public static int apuntador = -1;
     
-    public static void main (String [] args)
+    public static void main (String [] args) throws IOException
     {
         // Leer el archivo de texto línea por línea
         try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\rogel\\OneDrive\\Escritorio\\Semestre 8\\Lenguajes Y Autómatas II\\Proyecto\\src\\Recursos\\Tabla de Tokens2.txt")))
@@ -45,7 +46,6 @@ public class Vector_Código_Intermedio {
                 // Dividir la línea en partes
                 String[] partes = linea.split(",");
                 String palabra = partes[0];
-                
                 
                 // Reglas de análisis léxico
                 switch (palabra)
@@ -96,7 +96,45 @@ public class Vector_Código_Intermedio {
                             cintaDeVCI.set(apu, (apuntador + 1) + "");
                         }
                         break;
-                    case "operadores":
+                    case "*":
+                        verificarExistencia("*",palabra);
+                        break;
+                    case "/":
+                        verificarExistencia("/",palabra);
+                        break;
+                    case "+":
+                        verificarExistencia("+",palabra);
+                        break;
+                    case "-":
+                        verificarExistencia("-",palabra);
+                        break;
+                    case "<":
+                        verificarExistencia("<",palabra);
+                        break;
+                    case ">":
+                        verificarExistencia(">",palabra);
+                        break;
+                    case "<=":
+                        verificarExistencia("<=",palabra);
+                        break;
+                    case ">=":
+                        verificarExistencia(">=",palabra);
+                        break;
+                    case "==":
+                        verificarExistencia("==",palabra);
+                        break;
+                    case "not":
+                        verificarExistencia("not",palabra);
+                        break;
+                    case "and":
+                        verificarExistencia("and",palabra);
+                        break;
+                    case "or":
+                        verificarExistencia("or",palabra);
+                        break;
+                    case "=":
+                        verificarExistencia("=",palabra);
+                        break;
                 }
             }
         }
@@ -115,5 +153,124 @@ public class Vector_Código_Intermedio {
         cintaDeVCI.add(guardado);
         cintaDeVCIApuntador.add(apuntador++);
         guardado = null;
+    }
+    
+    public static void verificarExistencia(String temporal, String token)
+    {
+        String [] prioridad_60 = new String [] {"*", "/"};
+        String [] prioridad_50 = new String [] {"+", "-"};
+        String [] prioridad_40 = new String [] {"<", ">", "<=", ">=", "=="};
+        String [] prioridad_30 = new String [] {"not"};
+        String [] prioridad_20 = new String [] {"and"};
+        String [] prioridad_10 = new String [] {"or"};
+        String [] prioridad_0 = new String [] {"="};
+        
+        boolean encontrado = false;
+        
+        for (String elemento : prioridad_0)
+        {
+            if (temporal.equals(elemento))
+            {
+                operacionDePilas(temporal, 0, token);
+                encontrado = true;
+                break;
+            }
+        }
+        
+        for (String elemento : prioridad_10)
+        {
+            if (temporal.equals(elemento))
+            {
+                operacionDePilas(temporal, 10, token);
+                encontrado = true;
+                break;
+            }
+        }
+        
+        for (String elemento : prioridad_20)
+        {
+            if (temporal.equals(elemento))
+            {
+                operacionDePilas(temporal, 20, token);
+                encontrado = true;
+                break;
+            }
+        }
+        
+        for (String elemento : prioridad_30)
+        {
+            if (temporal.equals(elemento))
+            {
+                operacionDePilas(temporal, 30, token);
+                encontrado = true;
+                break;
+            }
+        }
+        
+        for (String elemento : prioridad_40)
+        {
+            if (temporal.equals(elemento))
+            {
+                operacionDePilas(temporal, 40, token);
+                encontrado = true;
+                break;
+            }
+        }
+        
+        for (String elemento : prioridad_50)
+        {
+            if (temporal.equals(elemento))
+            {
+                operacionDePilas(temporal, 50, token);
+                encontrado = true;
+                break;
+            }
+        }
+        
+        for (String elemento : prioridad_60)
+        {
+            if (temporal.equals(elemento))
+            {
+                operacionDePilas(temporal, 60, token);
+                encontrado = true;
+                break;
+            }
+        }
+        
+        if (encontrado == false)
+        {
+            cintaDeVCI.add(temporal);
+            cintaDeVCIApuntador.add(apuntador++);
+        }
+    }
+    
+    public static void operacionDePilas(String temporal, int prioridad, String token)
+    {
+        if (pilaDeOperadores.isEmpty())
+        {
+            pilaDeOperadores.push(temporal);
+            pilaDePrioridad.push(prioridad);
+            pilaDeOperadoresTokens.push(token);
+        }
+        else
+        {
+            if (pilaDePrioridad.lastElement() >= prioridad)
+            {
+                pilaDePrioridad.pop();
+                String respaldo = pilaDeOperadores.pop();
+                String respaldoToken = pilaDeOperadoresTokens.pop();
+                pilaDeOperadores.push(temporal);
+                pilaDePrioridad.push(prioridad);
+                pilaDeOperadoresTokens.push(token);
+                cintaDeVCI.add(respaldoToken);
+                cintaDeVCIApuntador.add(apuntador++);
+            }
+            else
+            {
+                pilaDeOperadores.push(temporal);
+                pilaDePrioridad.push(prioridad);
+                pilaDeOperadoresTokens.push(token);
+            }
+        }
     }
 }
